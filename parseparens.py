e7f24535b1b0b78d0eb4c9d3388be26e95f6c000
Lars Kellogg-Stedman <lars@seas.harvard.edu>
2013-01-07 12:44:39 -0500

    initial import

---
+++ parseparens.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python
+
+import os
+import sys
+import argparse
+import pprint
+
+def parse_args():
+    p = argparse.ArgumentParser()
+    return p.parse_args()
+
+class Parser(object):
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.data = {}
+        self.stack = []
+        self.acc = []
+        self.prev = []
+        self.cur = self.stack
+
+    def accumulate(self, x):
+        self.acc.append(x)
+
+    def acc_value(self):
+        val = ''.join(self.acc).strip()
+        self.acc = []
+        return val
+
+    def handle_open_paren(self):
+        v = self.acc_value()
+        if v:
+            self.cur.append(v)
+        newlist = []
+        self.cur.append(newlist)
+        self.prev.append(self.cur)
+        self.cur = newlist
+
+    def handle_close_paren(self):
+        v = self.acc_value()
+        if v:
+            self.cur.append(v)
+
+        self.cur = self.prev.pop()
+
+    def parse(self,data):
+        self.reset()
+
+        last_was_space=False
+
+        for c in data:
+            if not c.isspace():
+                last_was_space = False
+
+            if c == '(':
+                self.handle_open_paren()
+            elif c == ')':
+                self.handle_close_paren()
+            elif c.isspace():
+                if not last_was_space:
+                    self.accumulate(c)
+                last_was_space = True
+            else:
+                self.accumulate(c)
+
+def main():
+    opts = parse_args()
+
+    p = Parser()
+    p.parse(sys.stdin.read())
+
+    pprint.pprint(p.stack)
+
+if __name__ == '__main__':
+    main()
+
+
